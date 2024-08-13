0842e691f56400b7bf394e29d831204b7e849f8d
ququ2 <fedor.vinogradov@rt.ru>
2024-08-14 01:03:58 +0300

    go

--- app.js
+++ app.js
@@ -4,6 +4,5 @@ tg.ready();
 
 document.getElementById('checkBtn').addEventListener('click', () => {
     tg.sendData(JSON.stringify({ action: 'check_cod' }));
-    tg.showAlert("Запрос отправлен, ожидайте ответа!");
+    alert("Запрос отправлен, ожидайте ответа!");
 });
-
