45264ae76fae5dff7bab12a5a30116ce106dfbd1
ququ2 <fedor.vinogradov@rt.ru>
2024-08-14 01:21:38 +0300

    go

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
 document.getElementById('checkBtn').addEventListener('click', () => {
     tg.sendData(JSON.stringify({ action: 'check_cod' }));
-    console.log("Данные отправлены:", JSON.stringify({ action: 'check_cod' }));
     alert("Запрос отправлен, ожидайте ответа!");
 });
